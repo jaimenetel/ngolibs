af41f7d26d58364a94a7f4dc3e59aa7c5d72ddba
jaime-netel <156790521+jaime-netel@users.noreply.github.com>
2024-08-01 13:52:08 +0200

    urlcaller fluent

--- urlcaller/urlcaller.go
+++ urlcaller/urlcaller.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"sync"
-
-	ut "github.com/jaimenetel/ngolibs/urltools"
 )
 
 const (
@@ -54,7 +55,82 @@ func GetUrlCaller(url ...string) *UrlCaller {
 	})
 	return instance
 }
+func (uc UrlCaller) DoCallUrl(urlcall *UrlCall) string {
+	aurl := GetUrllCalAsJson(urlcall)
+	fmt.Println(aurl)
+	salida, err := _FetchURLPost(context.Background(), uc.UrlExecuter, aurl, "application/json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	salida = ReformatJSON(salida)
+	return salida
 
+}
+func NewUrlCaller(url ...string) *UrlCaller {
+	urlcall := URLIot
+	if len(url) > 0 {
+		urlcall = url[0]
+	}
+	return &UrlCaller{UrlExecuter: urlcall}
+}
+func NewUrlCall() *UrlCall {
+	return &UrlCall{
+		Params: make(map[string]string),
+	}
+}
+func (uc *UrlCall) SetUrl(url string) *UrlCall {
+	uc.UrlBase = url
+	return uc
+}
+func (uc *UrlCall) SetParams(params ...Prm) *UrlCall {
+	for _, prm := range params {
+		uc.Params[prm.K] = prm.V
+	}
+	return uc
+}
+func (uc *UrlCall) SetParam(k string, v string) *UrlCall {
+	uc.Params[k] = v
+	return uc
+}
+func (uc *UrlCall) SetAuthorization(auth string) *UrlCall {
+	uc.Authorization = auth
+	return uc
+}
+func (uc *UrlCall) SetApiKey(apikey string) *UrlCall {
+	uc.ApiKey = apikey
+	return uc
+}
+func (uc *UrlCall) SetBody(body string) *UrlCall {
+	uc.Body = body
+	return uc
+}
+func (uc *UrlCall) SetMethod(method string) *UrlCall {
+	uc.Method = method
+	return uc
+}
+func (uc *UrlCall) SetVeToken(vtoken bool) *UrlCall {
+	uc.VToken = vtoken
+	return uc
+}
+
+//	func (uc *UrlCall) CallUrl() string {
+//		abody, _ := json.MarshalIndent(uc, "", "  ")
+//		fmt.Println(string(abody))
+//		salida, err := _FetchURLPost(context.Background(), uc.UrlBase, string(abody), "application/json")
+//		if err != nil {
+//			fmt.Println(err)
+//		}
+//		salida = ReformatJSON(salida)
+//		return salida
+//	}
+func (uc *UrlCaller) SetUrl(url string) *UrlCaller {
+	uc.UrlExecuter = url
+	return uc
+}
+func GetUrllCalAsJson(uc *UrlCall) string {
+	abody, _ := json.MarshalIndent(uc, "", "  ")
+	return string(abody)
+}
 func GetUrl(urlbase string, prmList ...interface{}) string {
 	urlcall := UrlCall{UrlBase: urlbase, Params: map[string]string{}}
 
@@ -98,7 +174,7 @@ func GetUrl(urlbase string, prmList ...interface{}) string {
 func (uc *UrlCaller) CallUrl(url string, params ...interface{}) string {
 	abody := GetUrl(url, params...)
 	fmt.Println(abody)
-	salida, err := ut.FetchURLPostCtx(context.Background(), uc.UrlExecuter, abody, "application/json")
+	salida, err := _FetchURLPost(context.Background(), uc.UrlExecuter, abody, "application/json")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -107,33 +183,33 @@ func (uc *UrlCaller) CallUrl(url string, params ...interface{}) string {
 
 }
 
-// func _FetchURLPost(ctx context.Context, url string, body string, contentType string) (string, error) {
-// 	req, err := http.NewRequestWithContext(ctx, "POST", url, strings.NewReader(body))
-// 	if err != nil {
-// 		return "", fmt.Errorf("error al crear la solicitud POST: %v", err)
-// 	}
+func _FetchURLPost(ctx context.Context, url string, body string, contentType string) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, "POST", url, strings.NewReader(body))
+	if err != nil {
+		return "", fmt.Errorf("error al crear la solicitud POST: %v", err)
+	}
 
-// 	// Set the Content-Type header
-// 	req.Header.Set("Content-Type", contentType)
+	// Set the Content-Type header
+	req.Header.Set("Content-Type", contentType)
 
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return "", fmt.Errorf("error al hacer la solicitud POST: %v", err)
-// 	}
-// 	defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error al hacer la solicitud POST: %v", err)
+	}
+	defer resp.Body.Close()
 
-// 	if resp.StatusCode != http.StatusOK {
-// 		return "", fmt.Errorf("respuesta fallida con código de estado: %d", resp.StatusCode)
-// 	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("respuesta fallida con código de estado: %d", resp.StatusCode)
+	}
 
-// 	responseBody, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return "", fmt.Errorf("error al leer el cuerpo de la respuesta: %v", err)
-// 	}
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error al leer el cuerpo de la respuesta: %v", err)
+	}
 
-//		return string(responseBody), nil
-//	}
+	return string(responseBody), nil
+}
 func ReformatJSON(s string) string {
 	var f interface{}
 	err := json.Unmarshal([]byte(s), &f)
